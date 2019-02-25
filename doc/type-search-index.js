3b1b79e167bf7a697e7f8fa519867ea73905d998
hunterbowman <40217936+hunterbowman@users.noreply.github.com>
2019-02-24 22:21:10 -0700

    assignment2 part 1.c

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.asu.spring19.cse360.assignments.assignment1","l":"OrderedIntList"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.asu.spring19.cse360.assignments.assignment2","l":"Calculator"}]
--- src/edu/asu/spring19/cse360/assignments/assignment2/Calculator.java
+++ src/edu/asu/spring19/cse360/assignments/assignment2/Calculator.java
@@ -1,33 +1,61 @@
 package edu.asu.spring19.cse360.assignments.assignment2;
 
+/**
+ * @author hcbowman
+ *
+ */
 public class Calculator {
 
+	/**
+	 * 
+	 */
 	private int total;
 	
+	/**
+	 * 
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * @return
+	 */
 	public int getTotal () {
 		return 0;
 	}
 	
+	/**
+	 * @param value
+	 */
 	public void add (int value) {
 		
 	}
 	
+	/**
+	 * @param value
+	 */
 	public void subtract (int value) {
 		
 	}
 	
+	/**
+	 * @param value
+	 */
 	public void multiply (int value) {
 		
 	}
 	
+	/**
+	 * @param value
+	 */
 	public void divide (int value) {
 		
 	}
 	
+	/**
+	 * @return
+	 */
 	public String getHistory () {
 		return "";
 	}
